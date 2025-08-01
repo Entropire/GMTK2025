3c8ec3e3c3be15f0d0e796177376654c686ff47c
Entropire <158569522+Entropire@users.noreply.github.com>
2025-08-01 13:43:30 +0200

    added MainMenu and PauseMenu with lvl_0 and lvl_1 scenes

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+  [SerializeField] private GameObject PauseMenu;
+
+  public void SetScene(int sceneIndex)
+  {
+    SceneManager.LoadScene(sceneIndex);
+  }
+
+  public void Exit()
+  {
+    Application.Quit();
+  }
+
+  public void PauseGame(bool pause)
+  {
+    Time.timeScale = pause ? 0 : 1;
+  }
+
+  public void ShowPauseMenu(bool show)
+  {
+    if (PauseMenu != null)
+    {
+      PauseMenu.SetActive(show);
+    }
+  }
+
+  private void Update()
+  {
+    if (PauseMenu != null && Input.GetKeyDown(KeyCode.Escape))
+    {
+      PauseMenu.SetActive(!PauseMenu.active);
+    }
+  }
+}
