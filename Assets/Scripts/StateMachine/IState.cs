f815ed4fed1e15a5b7b6b1ef4bfdbb61c8ae2da4
Nxbi <noradegreeuw@gmail.com>
2025-10-18 17:23:19 +0200

    Removed the virtual from the functions from IState

--- Assets/Scripts/StateMachine/IState.cs
+++ Assets/Scripts/StateMachine/IState.cs
@@ -2,9 +2,9 @@
 {
     public interface IState
     {
-        public virtual void Enter() { }
-        public virtual void Exit() { }
-        public virtual void Update() { }
-        public virtual void FixedUpdate() { }
+        public void Enter() { }
+        public void Exit() { }
+        public void Update() { }
+        public void FixedUpdate() { }
     }
 }
